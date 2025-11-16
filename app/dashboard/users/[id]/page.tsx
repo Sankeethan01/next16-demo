0a125f0f897b210d433a6622a923fdc54e0db48c
Sankeethan01 <sankee26july@gmail.com>
2025-11-16 19:12:05 +0530

    practices custom layout creation

--- app/dashboard/users/[id]/page.tsx
+++ app/dashboard/users/[id]/page.tsx
@@ -1,9 +1,12 @@
+import Link from "next/link";
+
 const UserDetails = async ({ params } : { params : Promise<{ id : string }>}) => {
     const { id } = await params;
 
   return (
     <div>
-      hey There I am User #{id}
+      <p>hey There I am User #{id}</p>
+      <button><Link href={`/dashboard/users`}>Go Back</Link></button>
     </div>
   )
 }
