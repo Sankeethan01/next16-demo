47aa0a46703e4a6b7decbcb3ceb1f6b2fb219ea9
Sankeethan01 <sankee26july@gmail.com>
2025-11-16 10:30:20 +0530

    enables turbo pack file system caching for development to improve performance, as new feature of next 16

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    turbopackFileSystemCacheForDev: true,
+    /*Enable turbo pack file system cache for development to improve performance, 
+    as new feature of Next.js */ 
+  },
 };
 
 export default nextConfig;
