619f087c0778439adad79c06a6941ad9950bfd7f
Sankeethan01 <sankee26july@gmail.com>
2025-11-16 13:03:03 +0530

    installed react compiler and enabled in configuration to use built in memoization as new feature of next16

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  reactCompiler:true,
   experimental: {
     turbopackFileSystemCacheForDev: true,
     /*Enable turbo pack file system cache for development to improve performance, 
